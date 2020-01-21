3af542411c4f642c9bbfbbf309b08d6eee310657
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-21 09:55:16 -0700

    Starting graph project

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,13 +13,20 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # set is O(1) time complexity --> no duplicates. Better time complexity than using a list or an array
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
+
+        If both exist, add a connection from v1 to v2
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            # Python builtin exception --> 
+            raise IndexError("That vertex does not exits!")
 
     def get_neighbors(self, vertex_id):
         """
