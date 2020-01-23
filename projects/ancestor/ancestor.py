4d47b2da5327fdf2398cbda6a136f9ee09877386
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-23 10:32:45 -0700

    Adding changes from other files

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -56,6 +56,8 @@ Note:
 child = starting_node ==> starting_vertex
 ancestor = destination_node ==> destination_vertex
 
+
+# used DFS but the problem is asking for shortest path to oldest node. Use BFS when trying to find a path between to points
 PLAN:
     1. Use depth-first search approach template from ancestorTree.py
 
@@ -66,6 +68,7 @@ from util import  Stack, Graph
 
 ancestor_data = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
+# used DFS but the problem is asking for shortest path
 def earliest_ancestor(ancestors, starting_node):
     ancestorTree = Graph()
     # Iterate through ancestors to find vertices
